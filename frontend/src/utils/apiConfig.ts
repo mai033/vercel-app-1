b26f8f5a0a4082c714bfc89e607401ace2f824ec
mai033 <145622463+mai033@users.noreply.github.com>
2024-03-08 13:05:22 -0800

    change deployed api url

--- frontend/src/utils/apiConfig.ts
+++ frontend/src/utils/apiConfig.ts
@@ -1,4 +1,7 @@
 const LOCAL_API_BASE_URL = 'http://localhost:3005';
-const DEPLOYED_API_BASE_URL = 'link-to-blxd.vercel.app'; 
+const DEPLOYED_API_BASE_URL = 'vercel-app-1-rho.vercel.app';
 
-export const API_BASE_URL = window.location.hostname === 'localhost' ? LOCAL_API_BASE_URL : DEPLOYED_API_BASE_URL;
+export const API_BASE_URL =
+  window.location.hostname === 'localhost'
+    ? LOCAL_API_BASE_URL
+    : DEPLOYED_API_BASE_URL;
