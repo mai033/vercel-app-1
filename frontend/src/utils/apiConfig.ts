897b95c94cb51990f7aeb0a2767291efea652848
mai033 <145622463+mai033@users.noreply.github.com>
2024-03-08 13:25:16 -0800

    edit deployed api

--- frontend/src/utils/apiConfig.ts
+++ frontend/src/utils/apiConfig.ts
@@ -1,5 +1,5 @@
 const LOCAL_API_BASE_URL = 'http://localhost:3005';
-const DEPLOYED_API_BASE_URL = 'vercel-app-1-rho.vercel.app';
+const DEPLOYED_API_BASE_URL = 'https://vercel-app-1-rho.vercel.app';
 
 export const API_BASE_URL =
   window.location.hostname === 'localhost'
