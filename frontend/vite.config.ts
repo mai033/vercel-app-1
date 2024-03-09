9065c0437a444d19f81d416e4c912c67db26d8a4
mai033 <145622463+mai033@users.noreply.github.com>
2024-03-09 15:30:00 -0800

    upload to vercel blob

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,32 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     // Specify the output directory here
+//     outDir: 'build',
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 
-// https://vitejs.dev/config/
+// Initialize dotenv
+const myEnv = dotenv.config();
+dotenvExpand.expand(myEnv);
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Specify the output directory here
     outDir: 'build',
+  },
+  // Optionally, you can manually specify env variables here if needed.
+  // For example, to make sure they are correctly typed and available.
+  define: {
+    'process.env': process.env,
   },
 });
