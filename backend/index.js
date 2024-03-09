11d566c2e62faed0507f2ad1cb721e61ad703a63
mai033 <145622463+mai033@users.noreply.github.com>
2024-03-08 23:46:36 -0800

    commented out acl

--- backend/index.js
+++ backend/index.js
@@ -316,10 +316,10 @@ app.post('/generate-upload-url', async (req, res) => {
         ['content-length-range', 0, 10485760], // up to 10 MB
         ['starts-with', '$Content-Type', contentType],
       ],
-      Fields: {
-        acl: 'public-read',
-        'Content-Type': contentType,
-      },
+      // Fields: {
+      //   acl: 'public-read',
+      //   'Content-Type': contentType,
+      // },
       Expires: 600, // Seconds before the presigned post expires. 3600 by default.
     });
 
